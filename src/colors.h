a98184a36ef93a349f0ccfe282ef33b3cd6d6b96
Georgiy <goshakovalev5@gmail.com>
2025-05-01 13:19:28 +0400

    Header file for colouring the terminal

---
+++ src/colors.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define ESC "\033[0m"
+#define BLACK "\033[30m"
+#define WHITE "\033[97m"
+#define GREY "\033[90m"
+#define BLUE "\033[94m"
+#define ORANGE "\033[33m" 
+#define RED_FRONT "\033[101m"
+#define BLUE_FRONT "\033[46m" // темно-голубой
+#define ORANGE_FRONT "\033[103m"
+#define GREY_FRONT "\033[100m"
+#define WHITE_FRONT "\033[107m"
+#define BLACK_FRONT "\033[40m"
+
+#endif

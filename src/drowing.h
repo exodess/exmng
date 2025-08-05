b1799db15637308b9573c88014f4c61a201f0333
Georgiy <goshakovalev5@gmail.com>
2025-08-05 16:33:51 +0400

    Nothing update, just modified the comments

--- src/drowing.h
+++ src/drowing.h
@@ -1,11 +1,10 @@
 #ifndef DROWING_H
 #define DROWING_H
 
-#include <dirent.h>
 #include "list.h"
 
 void drowing_pole_file(int, int);
-void drowing_pole_dir(ListInfo_t *, int, int, int);
+void drowing_pole_dir(WindowInfo_t *, int, int, int);
 void drowing_pole_names(struct dirent *, int, int);
 void clear_pole(int, int, int);
 
